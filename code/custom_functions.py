43e7fd9bba192418f24cb0a13f8872204bbcb5fb
spycherf <spycher.frederic@gmail.com>
2019-12-10 11:01:27 +0100

    updated custom_split with PCA and poly

--- code/custom_functions.py
+++ code/custom_functions.py
@@ -1,13 +1,16 @@
 import pandas as pd
+import numpy as np
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 
 
-def custom_split(data, predict, rseed, scaling, encoding, features):
+def custom_split(data, predict, rseed, scaling, encoding, pca, poly, features):
     X = pd.DataFrame()
     y = data[predict]
     
@@ -40,7 +43,7 @@ def custom_split(data, predict, rseed, scaling, encoding, features):
         if encoding == "onehot":
             cat_oh = pd.DataFrame(one_hot.fit_transform(data[["category"]]).toarray())
             X = pd.concat((X, cat_oh), axis=1)
-        else:
+        elif encoding == "label":
             cat_le = pd.DataFrame(le.fit_transform(data["category"]), columns=["category"])
             X = pd.concat((X, cat_le), axis=1)
             
@@ -48,7 +51,7 @@ def custom_split(data, predict, rseed, scaling, encoding, features):
         if encoding == "onehot":
             main_cat_oh = pd.DataFrame(one_hot.fit_transform(data[["main_category"]]).toarray())
             X = pd.concat((X, main_cat_oh), axis=1)
-        else:
+        elif encoding == "label":
             main_cat_le = pd.DataFrame(le.fit_transform(data["main_category"]), columns=["main_category"])
             X = pd.concat((X, main_cat_le), axis=1)
             
@@ -56,10 +59,19 @@ def custom_split(data, predict, rseed, scaling, encoding, features):
         if encoding == "onehot":
             country_oh = pd.DataFrame(one_hot.fit_transform(data[["country"]]).toarray())
             X = pd.concat((X, country_oh), axis=1)
-        else:
+        elif encoding == "label":
             country_le = pd.DataFrame(le.fit_transform(data["country"]), columns=["country"])
             X = pd.concat((X, country_le), axis=1)
             
+    if pca != False:
+        pca_fitted = PCA(n_components=pca).fit_transform(X)
+        X = pd.DataFrame(data=pca_fitted[:,:2])
+        
+    if poly != False:
+        polynomial = PolynomialFeatures(poly)
+        X = polynomial.fit_transform(np.array(X))
+        y = np.array(y)
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=rseed)
     
     return X_train, X_test, y_train, y_test
